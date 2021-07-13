67d8645054f8c48231f3554a39b34edfbbc9cb18
markh1089 <markhempel89@gmail.com>
2021-07-13 10:40:04 +0100

    added icons

--- weatherapi/src/components/WeatherCard.js
+++ weatherapi/src/components/WeatherCard.js
@@ -10,10 +10,11 @@ const WeatherCard = ({ time }) => {
 
   //weather details
   let day = moment(dateFormat).format("dddd")
-  let date = moment().format('MMMM Do YYYY, h:mm a') 
+  let date = moment().format('MMMM Do YYYY') 
   let temp = Math.round(time.main.temp)
   let weatherDescription = time.weather[0].description
-  console.log(weatherDescription)
+  let iconCode = time.weather[0].icon
+  let weatherLink = `http://openweathermap.org/img/wn/${iconCode}.png`
   
 
   return (
@@ -22,6 +23,7 @@ const WeatherCard = ({ time }) => {
         <h3 className="card-title">{day}</h3>
         <p>{date}</p>
         <h2>{temp} °C</h2>
+        <img className='icon' src={weatherLink} alt='icon' />
         <p className='weatherDescription'>{weatherDescription}</p>
       </div>
     </div>
